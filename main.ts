f7f85d9422e85aa32918b034299e83a0f3f2a222
lucas-nguyen0 <lucas.nguyen@mths.ca>
2024-10-16 11:26:34 -0400

    program can take in light, and represent how much it's taken in

--- main.ts
+++ main.ts
@@ -1,8 +1,58 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Lucas Nguyen
+ * Created on: Oct 2024
+ * This program can take in light, and represent how much it's taken in
 */
 
-basic.showString('Hello, World!')
+let neopixelStrip: neopixel.Strip = null
+let inputLightLevel: number = 0
+
+
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    neopixelStrip.show()
+    inputLightLevel = input.lightLevel()
+    basic.pause(1000)
+
+    // no neopixel
+    if (inputLightLevel <= 51) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+    }
+
+    // first neopixel
+    if (inputLightLevel > 52) {
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Blue))
+    }
+
+    // second neopixel
+    if (inputLightLevel > 104) {
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+    }
+
+    // third neopixel
+    if (inputLightLevel > 156) {
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Purple))
+    }
+
+    // fourth neopixel
+    if (inputLightLevel > 208) {
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Orange))
+    }
+    neopixelStrip.show()
+})
